e83e0d9351ba3b149b44e21c043fa6808348f3aa
Muntha <munthasir911@gmail.com>
2021-10-20 21:44:06 +0530

    Implemented comment feature

--- Screens/SearchedProfile.js
+++ Screens/SearchedProfile.js
@@ -1,26 +1,26 @@
 import { ScrollView } from 'native-base';
-import React, {useEffect, useState } from 'react';
-import {Text, View, StyleSheet, Image, FlatList, Dimensions, Modal, Button,TextInput, TouchableWithoutFeedback, Keyboard} from 'react-native'
+import React, {useEffect, useState, useContext } from 'react';
+import {Text, View, StyleSheet, Image, FlatList, Dimensions, Modal, Button,TextInput, TouchableWithoutFeedback, Keyboard, ActivityIndicator} from 'react-native'
 import { backgroundColor, borderRadius, maxHeight, padding, width } from 'styled-system';
 import { background } from 'styled-system';
 import SearchedProfileHeader from '../Components/SearchedProfileHeader';
 import Card from '../Components/Card';
+import { CredintialsContext } from '../Components/CredintialContext';
 export default function SearchedProfile({ route, navigation}){
 
-    const {searchedId} = route.params
+    const {storedCredintials, setStoredCredintials} = useContext(CredintialsContext)
+    const {user_email, user_username, _id} = storedCredintials
+
+    const {searchedId} = route.params //visiting profile id
     const [userDetails, setUserDetails] = useState('')
     const [posts, setPosts] = useState('')
+    const [loading, setLoading] = useState(false)
     
     const [showModal, setShowModal] = useState(false)
     const [newComment, setNewComment] = useState('')
-    const [comment, setComments] = useState([
-        { id: '1', name: "jason", comment: 'very polite dealer'},
-        { id: '2', name: "jason", comment: 'very polite dealer'},
-        { id: '3', name: "jason", comment: 'very polite dealer'},
-        { id: '4', name: "jason", comment: 'very polite dealer'},
-        { id: '5', name: "jason", comment: 'very polite dealer'},
-        
-    ])
+    const [comment, setComments] = useState([])
+    const [commentVisible, setCommentVisible] = useState(false)
+
 
     const vehiDetails = () => {
 
@@ -38,17 +38,59 @@ export default function SearchedProfile({ route, navigation}){
         })
         .then((response) => response.json())
         .then((response) => {
-            console.log(response.posts)
             setPosts(response.posts)
+            
 
             
         })
         .catch((error) => {
             console.log(error)
         })
+        .finally(() => setLoading(false))
         
+        setLoading(true)
+
        
     }
+
+const addComment = () => {
+
+    const commentOnId = searchedId;
+    const commentedBy = user_username;
+    const commentedById = _id;
+    const comment = newComment;
+
+    
+    fetch('http://192.168.1.15:3000/general/addComment', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            commentedOnId : commentOnId,
+            commentedBy : commentedBy,
+            commentedById : commentedById,
+            comment: comment
+        })
+    })
+    .then((response) => response.json())
+    .then((response) => {
+       
+        console.log(response.result)
+        
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+    
+
+}
+
+
+    
 
     useEffect(() => {
 
@@ -64,73 +106,106 @@ export default function SearchedProfile({ route, navigation}){
         })
         .then((response) => response.json())
         .then((response) => {
-            console.log(response.user)
+           // console.log(response.user)
             setUserDetails(response.user)
+            console.log(response.user.comments.length)
+            setComments(response.user.comments)
 
+            if(response.user.comments.length > 0) {
+                setCommentVisible(true)
+            }
+
+            
             
         })
         .catch((error) => {
             console.log(error)
         })
-        //.finally(() => isLoading(false))
+        .finally(() => setLoading(false))
 
         vehiDetails()
 
+        setLoading(true)
+
+
       },[]);
 
-
-    return(
+      if(loading){
+            return(
+                <ActivityIndicator/>
+            )
+      }
+      else
+      {
+        return(
         
-        <View style={styles.container}>
-            <Modal visible={showModal}  animationType="slide">
-                <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                    <View style={styles.modalContainer}>
-                        <View style={styles.modalHeaderTextContainer}>
-                            <Text style={styles.modalHeaderText}>Write a Review</Text>
-                            <Button title="add" onPress={() => {comment.push(newComment)}}/>
-
+            <View style={styles.container}>
+                <Modal visible={showModal}  animationType="slide">
+                    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                        <View style={styles.modalContainer}>
+                            <View style={styles.modalHeaderTextContainer}>
+                                <Text style={styles.modalHeaderText}>Write a Review</Text>
+                                <Button title="add" onPress={addComment}/>
+    
+                            </View>
+                            <View style={{}}>
+                                <TextInput
+                                multiline
+                                placeholder="Write review"
+                                numberOfLines={4}
+                                    onChangeText={(val) => setNewComment(val)}
+                                style={styles.modalInput}
+                                />
+                            </View>
+                            <Button title="close" onPress={() => setShowModal(false)}/>
                         </View>
-                        <View style={{}}>
-                            <TextInput
-                            multiline
-                            numberOfLines={4}
-                                onChangeText={(val) => setNewComment(val)}
-                            style={styles.modalInput}
-                            />
-                        </View>
-                        <Button title="close" onPress={() => setShowModal(false)}/>
-                    </View>
-                </TouchableWithoutFeedback>
-            </Modal>
-
-           <View style={styles.headerContainer}>
-               <SearchedProfileHeader onPress={() => setShowModal(true)} postCount={userDetails.no_of_posts} />
-           </View>
-           <View style={{flex: 2}} >
-               <FlatList
+                    </TouchableWithoutFeedback>
+                </Modal>
+    
+               <View style={styles.headerContainer}>
+                   <SearchedProfileHeader onPress={() => setShowModal(true)} postCount={userDetails.no_of_posts} />
+               </View>
+               <View style={{flex: 2, }} >
+                
+                {commentVisible?
+                
+                <FlatList
                     data={comment}
                     horizontal={true}
-                    keyExtractor={(item) => item.id}
+                    keyExtractor={(item) => item._id}
                     pagingEnabled={true}
                     showsHorizontalScrollIndicator={false}
                     renderItem={({item}) => (
                         <View style={styles.reviewContaineer}>
-                                <Text>{item.comment}</Text>
+                                <Text style={styles.commentedByText}>{item.commentedBy} </Text>
+                                <Text style={styles.commentText}>{item.comment}</Text> 
                         </View>
                     )}
-                />
-           </View>
-           <View style={styles.postContainer}>
-               <FlatList
-                data={posts}
-                keyExtractor={(item) => item._id}
-                renderItem={({item}) => (
-                    <Card model={item.model} postedBy={item.postedBy} image={item.image} />
-                )}
-             />
-           </View>
-        </View>
-    )
+                /> :
+
+                <View style={styles.reviewContaineer}>
+                    <Text style={styles.commentText}>No Reviews</Text>
+                </View>
+             } 
+             
+
+                
+                   
+               </View>
+               <View style={styles.postContainer}>
+                   <FlatList
+                    data={posts}
+                    keyExtractor={(item) => item._id}
+                    renderItem={({item}) => (
+                        <Card model={item.model} postedBy={item.postedBy} image={item.image} />
+                    )}
+                 />
+               </View>
+            </View>
+        )
+      }
+
+    
 }
 
 
@@ -193,6 +268,14 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         padding: 5
 
+    },
+    commentedByText: {
+        fontFamily: 'Helvetica-Bold',
+        fontSize: 18
+    },
+    commentText: {
+        fontFamily: 'Helvetica',
+        fontSize: 15
     }
 
 })
